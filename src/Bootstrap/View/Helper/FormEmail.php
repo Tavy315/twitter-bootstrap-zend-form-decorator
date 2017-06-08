90e8120a98bb25db06432edf4840c3a76b0c4e1f
Octav <tavy315@users.noreply.github.com>
2017-06-08 11:42:51 +0300

    Update FormEmail.php

--- src/Bootstrap/View/Helper/FormEmail.php
+++ src/Bootstrap/View/Helper/FormEmail.php
@@ -19,7 +19,7 @@ class FormEmail extends FormText
      * @param mixed        $value   The element value
      * @param array        $attribs Attributes for the element tag
      *
-     * @return string The element XHTML.
+     * @return string The element XHTML
      */
     public function formEmail($name, $value = null, $attribs = null)
     {
