c88d9ac86b3c436b671a671be4ba2a9e3b4d58e6
Octav <tavy315@users.noreply.github.com>
2017-06-08 11:36:41 +0300

    Create FormNumber.php

--- src/Bootstrap/View/Helper/FormNumber.php
+++ src/Bootstrap/View/Helper/FormNumber.php
@@ -15,9 +15,9 @@ class FormNumber extends FormText
      *
      * @param string|array $name    If a string, the element name.  If an
      *                              array, all other parameters are ignored, and the array elements
-     *                              are used in place of added parameters.
-     * @param mixed        $value   The element value.
-     * @param array        $attribs Attributes for the element tag.
+     *                              are used in place of added parameters
+     * @param mixed        $value   The element value
+     * @param array        $attribs Attributes for the element tag
      *
      * @return string The element XHTML.
      */
